adbd7e5e9bebddf3d35b2f4ef3c7aba15a712804
qasx135 <qasx135@gmail.com>
2025-01-02 22:44:12 +0300

    Done before algorythms

--- cmd/vlc.go
+++ cmd/vlc.go
@@ -1,13 +1,70 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
 
 var vlcCmd = &cobra.Command{
 	Use: "vlc",
 	Short: "Pack file using variable-length code",
-	Run: 
+	Run: pack,
 }
 
+const packedExtension = "vlc"
+
+var ErrEmptyPath = errors.New("path to file is not specified")
+
 func pack(_ *cobra.Command, args []string) {
+	if len(args) == 0 || args[0] == "" {
+		handleErr(ErrEmptyPath)
+	} 
 	filePath := args[0]
+
+	r, err := os.Open(filePath)
+	if err != nil {
+		handleErr(err)
+	}
+	defer r.Close()
+
+	data, err := io.ReadAll(r)
+	if err != nil {
+		handleErr(err)
+	}
+
+	// packed := Encode(data)
+	packed := "" // TODO: remove
+	fmt.Println(string(data))
+
+	err = os.WriteFile(packedFileName(filePath), []byte(packed), 0644)
+	if err != nil {
+		handleErr(err)
+	}
+}
+
+func packedFileName(path string) string {
+	//----------simple mod---------\\
+
+	// // path = /path/to/file/myFile.txt
+	// fileName := filepath.Base(path) // fileName = myFile.txt
+	// ext := filepath.Ext(fileName) // ext = .txt
+	// baseName := strings.TrimSuffix(fileName, ext) // baseName = myFile
+
+	// return baseName + "." + packedExtension
+
+	//----------work mod---------\\
+
+	fileName := filepath.Base(path)
+	return strings.TrimSuffix(fileName, filepath.Ext(fileName)) + "." + packedExtension
+
+}
+
+func init() {
+	packCmd.AddCommand(vlcCmd)
 }
--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package archiver
+package main
 
 import "archiver/cmd"
 
